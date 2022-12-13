2c829d4d4a16d977e6b72de18e3b1c6fa65b2a6d
samanthawanjiru <samantha.wanjiru03@gmail.com>
2022-12-13 09:18:15 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include <main.h> 
 /**
  * main-print putchar followed by a new line
  *
@@ -6,14 +6,8 @@
  */
 int main(void) 
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	_putchar("_putchar");
+
 	_putchar('\n');
 	
 	return (0) ;
