9788f048921cfe867d4c6f3b5f7d7d720499b09a
samanthawanjiru <samantha.wanjiru03@gmail.com>
2022-12-13 09:07:30 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,20 @@
 /**
  * main-print putchar followed by a new line
  *
- *Return :always 0
+ *Return :Always 0
  */
 int main(void) 
 {
-	 _putchar('_' );
-	 _putchar ('p' );
-	 _putchar ('u' );
-	 _putchar ('t' );
-	 _putchar ('c' );
-	 _putchar ('h' );
-	 _putchar ('a' );
-	 _putchar ('r' );
-	 _putchar ('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
+	return (0) ;
 
-	  return (0) ;
 } 
