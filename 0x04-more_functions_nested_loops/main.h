a13600a6ca40a5a6b1fd6d5821fcf4244629e75a
samanthawanjiru <samantha.wanjiru03@gmail.com>
2022-12-16 12:53:21 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: Samantha Wanjiru
+ * Desc: Header contains prototypes for all functions
+ *       used in 0x03-more_functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+
+
+
+
+#endif
